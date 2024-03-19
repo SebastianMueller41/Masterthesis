b67586470566c279238a51a9fb84599e0ca1b6dc
SebastianMueller41 <mueller-inbox@outlook.de>
2024-03-19 23:35:07 +0100

    Added sliding window strategy for expand phase

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import os
 import sys
 import time
 import resource
+import argparse
 from src.search.hybrid import HybridSearch
 from src.search.bfs import BFS
 from src.solver.kernelsolver import KernelSolver
@@ -27,15 +28,21 @@ from src.database.database import create_connection, log_execution_data
 parser = argparse.ArgumentParser(description='Run the kernelization process with optional database logging.')
 parser.add_argument('dataset_file', type=str, help='Path to the dataset file')
 parser.add_argument('strategy_param', type=int, help='Strategy parameter value')
+parser.add_argument('--sw-size', '--sliding-window', type=int, default=1, help='Define the window size for the sliding-window technique (default: 1)')
+parser.add_argument('-dc', '--divide-conquer', action='store_true', help='Activate the divide and conquer technique')
 parser.add_argument('--log-db', action='store_true', help='Enable logging to database')
 
 # Parse arguments
 args = parser.parse_args()
 
+# Function to read dataset and return its content
 def read_dataset_content(dataset_filepath):
-    with open(dataset_filepath, 'r') as file:
-        return file.read()
-
+    try:
+        with open(dataset_filepath, 'r') as file:
+            return [line.strip() for line in file.readlines()]
+    except FileNotFoundError:
+        sys.exit(f"File {dataset_filepath} not found.\nPlease check the file path.")
+    
 if __name__ == "__main__":
     create_connection()  # Prepare the database
     start_time = time.time()
@@ -44,15 +51,20 @@ if __name__ == "__main__":
         print(f"Dataset file not found: {args.dataset_file}")
         sys.exit(1)
 
+    # Read the dataset
     dataset_content = read_dataset_content(args.dataset_file)
+
+    # Validate the sliding window size
+    if not 1 <= args.sw_size <= len(dataset_content):
+        sys.exit(f"--sw-size/--sliding-window must be between 1 and the length of the dataset ({len(dataset_content)}).")
 
     hitting_set_tree = None  # Initialize the hitting set tree to None
 
     try:
         if args.strategy_param == 0:
-            hitting_set_tree = KernelSolver(BFS(ExpandShrink(), DataSet(args.dataset_file, args.strategy_param), "A1")).solve()
+            hitting_set_tree = KernelSolver(BFS(ExpandShrink(args.sw_size,args.divide_conquer), DataSet(args.dataset_file, args.strategy_param), "A1")).solve()
         elif 0 < args.strategy_param < 4:
-            hitting_set_tree = KernelSolver(HybridSearch(ExpandShrink(), DataSet(args.dataset_file, args.strategy_param), "A1", args.strategy_param)).solve()
+            hitting_set_tree = KernelSolver(HybridSearch(ExpandShrink(args.sw_size,args.divide_conquer), DataSet(args.dataset_file, args.strategy_param), "A1", args.strategy_param)).solve()
         else:
             print("WRONG STRATEGY PARAM! MUST BE 0 = no B&B, 1 = Cardinality, 2 = Random, 3 = Inconsistency")
             sys.exit(1)
