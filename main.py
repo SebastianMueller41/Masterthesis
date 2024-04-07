a3b7a7885d26432e965f96c964424b5f0b0268c7
SebastianMueller41 <mueller-inbox@outlook.de>
2024-04-07 12:54:22 +0200

    added alpha as argument

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ parser.add_argument('strategy_param', type=int, help='Strategy parameter value')
 parser.add_argument('--sw-size', '--sliding-window', type=int, default=1, help='Define the window size for the sliding-window technique (default: 1)')
 parser.add_argument('-dc', '--divide-conquer', action='store_true', help='Activate the divide and conquer technique')
 parser.add_argument('--log-db', action='store_true', help='Enable logging to database')
+parser.add_argument('--alpha', type=str, help='A string value to be used as alpha')
 
 # Create a mutually exclusive group for the -kernel and -remainder arguments
 group = parser.add_mutually_exclusive_group()
@@ -68,14 +69,17 @@ if __name__ == "__main__":
     # Validate the sliding window size
     if not 1 <= args.sw_size <= len(dataset_content):
         sys.exit(f"--sw-size/--sliding-window must be between 1 and the length of the dataset ({len(dataset_content)}).")
+    
+    if args.alpha:
+        print(f"Alpha: {args.alpha}")
 
     hitting_set_tree = None  # Initialize the hitting set tree to None
 
     try:
         if args.strategy_param == 0:
-            hitting_set_tree = KernelSolver(BFS(ExpandShrink(args.sw_size,args.divide_conquer), DataSet(args.dataset_file, args.strategy_param), "arg_0&&!arg_0")).solve()
+            hitting_set_tree = KernelSolver(BFS(ExpandShrink(args.sw_size,args.divide_conquer), DataSet(args.dataset_file, args.strategy_param), args.alpha)).solve()
         elif 0 < args.strategy_param < 4:
-            hitting_set_tree = KernelSolver(HybridSearch(ExpandShrink(args.sw_size,args.divide_conquer), DataSet(args.dataset_file, args.strategy_param), "arg_0&&!arg_0", args.strategy_param)).solve()
+            hitting_set_tree = KernelSolver(HybridSearch(ExpandShrink(args.sw_size,args.divide_conquer), DataSet(args.dataset_file, args.strategy_param), args.alpha, args.strategy_param)).solve()
         else:
             print("WRONG STRATEGY PARAM! MUST BE 0 = no B&B, 1 = Cardinality, 2 = Random, 3 = Inconsistency")
             sys.exit(1)
