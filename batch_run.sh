c321c6680a5235b9ed54d1b6bebb47e5bb9740a0
SebastianMueller41 <mueller-inbox@outlook.de>
2024-07-23 02:23:22 +0200

    changed batch

--- batch_run.sh
+++ batch_run.sh
@@ -5,7 +5,7 @@ PYTHON_SCRIPT="main.py"
 
 # List of CSV files containing missing combinations
 MISSING_COMBINATION_FILES=(
-    'data/SRS/missingsig3_5_15.csv'
+    'data/SRS/sig5_15_25.csv'
     #'data/SRS/sig5_15_25.csv'
     #'data/SRS/sig10_15_25.csv'
     #'data/SRS/SRS/Dataset_A/sig15_15_25.csv'
@@ -13,8 +13,8 @@ MISSING_COMBINATION_FILES=(
 )
 
 # Strategy parameters and corresponding pruner combinations
-STRATEGY_PARAMS=(2)
-PRUNER_OPTIONS=(LOWER UPPER)
+STRATEGY_PARAMS=(1 2)
+PRUNER_OPTIONS=("LOWER")
 
 # Search strategies
 SEARCH_STRATEGIES=('PBS')
