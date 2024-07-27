cd3ab2148e4c33c5bed2d555ae4006c9a84e30ed
SebastianMueller41 <mueller-inbox@outlook.de>
2024-07-27 14:29:57 +0200

    Changed interface

--- src/search/strategy.py
+++ src/search/strategy.py
@@ -4,6 +4,3 @@ class Strategy(ABC):
     @abstractmethod
     def find_kernels(self) -> None:
         pass
-
-    def methodForAll(self) -> None:
-        print("method that is inherited by strategies")
